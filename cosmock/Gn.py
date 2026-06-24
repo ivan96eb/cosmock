6795a797824711c620050f22a81ffb528ed0f9ed
Sebastian <ssage@arizona.edu>
2026-06-24 11:15:59 -0700

    remove struct from Gn

--- cosmock/Gn.py
+++ cosmock/Gn.py
@@ -1,8 +1,6 @@
 import numpy as np
-from .structs import NonlinParameters
 
-
-def Gn(x, params):
+def Gn(x, N, lbda):
     """
     Evaluate Gn transformation at given x values
     
@@ -19,15 +17,15 @@ def Gn(x, params):
     y : transformed values
     """
     # Evaluate transformation
-    if params.N == 2:
-        alpha, beta = params.lbda
+    if N == 2:
+        alpha, beta = lbda
         return beta * np.exp(alpha * x - 0.5 * alpha**2) - beta
     
-    elif params.N == 3:
-        a, b, c = params.lbda
+    elif N == 3:
+        a, b, c = lbda
         arg = np.exp(a * x - 0.5 * a**2) + b*x + c
         norm = 1/(1+c)
         return norm * arg - 1
     
     else:
-        raise ValueError(f"Unknown model type: {params.N}")
+        raise ValueError(f"Unknown model type: {N}")
